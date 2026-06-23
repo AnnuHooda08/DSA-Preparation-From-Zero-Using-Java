4306432fce49c8fb8f4a6a150f127ed745e81cf5
AnnuHooda08 <anuhooda08@gmail.com>
2026-06-23 21:11:07 +0530

    adding loops topic

---
+++ Java Programming/04 - Loops/evenOdd.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class evenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("enter number of integer you want to know about:");
+        
+        int a = sc.nextInt();
+
+        while (a>0){
+            System.out.println("enter number:");
+
+            int num = sc.nextInt();
+            if(num % 2 == 0){
+                System.out.println("number is even");
+            }else{
+                System.out.println("number is odd");
+            }
+
+            a--;
+        }
+    }
+}
---
+++ Java Programming/04 - Loops/whileLoop.java
@@ -0,0 +1,9 @@
+public class whileLoop {
+    public static void main(String[] args) {
+        int a = 5;  // initialization
+        while(a++<10){  // condition 
+            System.out.println(a);
+            a++; // increment
+        }
+    }
+}
