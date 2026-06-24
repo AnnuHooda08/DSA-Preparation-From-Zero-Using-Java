e14361965a7ec04c3cde5257c35959c57510b103
AnnuHooda08 <anuhooda08@gmail.com>
2026-06-24 23:42:19 +0530

    solved leetcode question 1342

---
+++ Java Programming/Leetcode Questions/SteptoReduceNumber1342.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class SteptoReduceNumber1342 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("enter number: ");
+        int num = sc.nextInt();
+        int step = 0;
+        while(num > 0){
+            if(num % 2 == 0){
+                num /= 2;
+            }else{
+                num -= 1;
+            }
+            step++;
+        }
+        System.out.println("number of steps are: " + step);
+    }
+}
