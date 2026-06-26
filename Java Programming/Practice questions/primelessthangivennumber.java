06ebc599c2ee54524eb4812a8460920003419d00
AnnuHooda08 <anuhooda08@gmail.com>
2026-06-26 22:19:04 +0530

    added practice questions

--- Java Programming/Practice questions/primelessthangivennumber.java
+++ Java Programming/Practice questions/primelessthangivennumber.java
@@ -1,23 +1,24 @@
 import java.util.*;
+
 public class primelessthangivennumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter number: ");
         int num = sc.nextInt();
 
-        boolean isPrime = true;
-           
-        for(int i = 2; i*i <= num; i++){
-            if(num % i == 0){
-                isPrime = false;
-                break;
+        for (int n = 2; n < num; n++) {
+            boolean isPrime = true;
+
+            for (int i = 2; i * i <= n; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(n + " ");
             }
         }
-
-        if(isPrime == true){
-            System.out.println("this number is prime ");
-        }else{
-            System.out.println("number is not prime ");
-        }
     }
-    
 }
