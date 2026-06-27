e29972fd94ba54a6657d38b985deeeaebbf4d54d
AnnuHooda08 <anuhooda08@gmail.com>
2026-06-27 22:32:08 +0530

    added practice questions

--- Java Programming/Practice questions/countDigit.java
+++ Java Programming/Practice questions/countDigit.java
@@ -1,6 +1,16 @@
 import java.util.*;
 public class countDigit {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner (System.in);
+        System.out.print("enter number:");
+        int n = sc.nextInt();
+        int count = 0;
+        while (n > 0){
+            int digit = n % 10;
+            n /=10;
+            count++;
+        }
+        System.out.println("number of digit are : " + count);
+        sc.close();
     }
 }
